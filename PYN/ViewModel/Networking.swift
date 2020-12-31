a8bbff14060b76476b2135cf623d5a227958b55d
Awaleh Hassan <awaleh_h@yahoo.com>
2020-12-31 14:54:48 +0100

    Made some changes to the networking class, made the session wait for connection or retry once  after a connection fail.

--- PYN/ViewModel/Networking.swift
+++ PYN/ViewModel/Networking.swift
@@ -6,31 +6,38 @@
 //
 
 import Foundation
-import UIKit
+//import UIKit
 import Combine
 
 
 class Networking {
     
-    deinit {
-        print("Networking object  deallocated!!!!")
-    }
     
-    func fetchData(query: String, language: Languages) -> AnyPublisher<[CompleteArticle], Error>{
+    private lazy var session: URLSession = {
+        let config = URLSessionConfiguration.default
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForResource = 300
+        let session = URLSession(configuration: config)
+        return session
+    }()
+    
+    
+    func fetchData(query: String, language: Language) -> AnyPublisher<[CompleteArticle], NetworkError>{
         
         
         guard let url = self.prepareUrlFor(query, for: language) else {
             fatalError("Url Not Found")
         }
         
-        return  URLSession.shared.dataTaskPublisher(for: url)
+        return  self.session.dataTaskPublisher(for: url)
+                            .retry(1)
                             .tryMap{ result in
-                                guard let response = result.response as? HTTPURLResponse else {throw NetworkError.network}
-                                guard (200..<300).contains(response.statusCode) else { throw NetworkError.network}
+                                guard let response = result.response as? HTTPURLResponse,
+                                      (200..<300).contains(response.statusCode) else {throw NetworkError.badServerResponse}
                                 return result.data
                             }
                             .decode(type:SearchResult.self, decoder: JSONDecoder())
-                            .mapError({NetworkError(error: $0, query: query)})
+                            .mapError{NetworkError.init(error: $0)}
                             .map(\.articles)
                             .map{ (article: [RawArticle]) -> ([RawArticle], String, Language) in
                                 return (article, query, language)
