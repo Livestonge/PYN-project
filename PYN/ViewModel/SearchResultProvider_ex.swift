f8891b0daae2b7629813672701f1da47a3c136e1
Moussa Hassan <awaleh_h@yahoo.com>
2021-06-08 00:34:10 +0200

    Nested type for a single source of truth

---
+++ PYN/ViewModel/SearchResultProvider_ex.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultProvider_ex.swift
+//  PYN
+//
+//  Created by Awaleh  on 06/06/2021.
+//
+
+import Foundation
+
+
+extension SearchResultProvider{
+    
+    enum State {
+        case isPending, isShowingLanguage, isLoading
+    }
+}
